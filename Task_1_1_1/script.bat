1a821f9074c2379205ff76f84dcadeab3ef00fd3
vhlebnikov <91516875+vhlebnikov@users.noreply.github.com>
2022-09-23 23:58:31 +0700

    Update script.bat

--- Task_1_1_1/script.bat
+++ Task_1_1_1/script.bat
@@ -1,6 +1,6 @@
 javadoc -d .\make\ -sourcepath .\src\main\java -subpackages ru.nsu.khlebnikov
 
-javac -d .\make\bin\ -sourcepath .\src\main\java .\src\main\java\ru\nsu\ablaginin\Heapsort.java
+javac -d .\make\bin\ -sourcepath .\src\main\java .\src\main\java\ru\nsu\khlebnikov\HeapSort.java
 
 mkdir .\make\jar
-jar cf .\make\jar\Heapsort.jar -C .\make\bin .
+jar cf .\make\jar\HeapSort.jar -C .\make\bin .
