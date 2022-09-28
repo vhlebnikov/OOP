9237c7736ef200e13190e0c3079bedfd93ccf9a8
vhlebnikov <v.khlebnikov@g.nsu.ru>
2022-09-28 14:07:07 +0700

    Add string test

--- Task_1_2_1/src/test/java/ru/nsu/khlebnikov/StackTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/khlebnikov/StackTest.java
@@ -71,4 +71,22 @@ public class StackTest {
         Stack<Integer> expected = new Stack<Integer>(0);
         Assertions.assertTrue(stack.stackAssertion(expected));
     }
+
+    @Test
+    public void stringStackTest() {
+        Stack<String> expected = new Stack<String>(1);
+        expected.push("2");
+
+        Stack<String> stack = new Stack<String>(4);
+        stack.push("2");
+        stack.push("7");
+        Stack<String> s1 = new Stack<String>(2);
+        s1.push("4");
+        s1.push("8");
+        stack.pushStack(s1);
+        stack.pop();
+        stack.popStack(2);
+        stack.count();
+        Assertions.assertTrue(stack.stackAssertion(expected));
+    }
 }
