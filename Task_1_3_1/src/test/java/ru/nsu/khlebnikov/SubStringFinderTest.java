0df79c8a058b51668e728f6f7c59b9d40b02c87e
vhlebnikov <v.khlebnikov@g.nsu.ru>
2022-11-17 05:33:58 +0700

    It works, in my opinion, well

--- Task_1_3_1/src/test/java/ru/nsu/khlebnikov/SubStringFinderTest.java
+++ Task_1_3_1/src/test/java/ru/nsu/khlebnikov/SubStringFinderTest.java
@@ -1,31 +1,62 @@
 package ru.nsu.khlebnikov;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Tests for my substring finder implementation.
+ */
 public class SubStringFinderTest {
+
     @Test
-    public void exceptionTest() {
-        Throwable exception = assertThrows(NullPointerException.class, () -> SubStringFinder.findSubString("input.txt", "ba"));
-        assertEquals(exception.getMessage(), "File is empty");
-        exception = assertThrows(IOException.class, () -> SubStringFinder.findSubString("wrong_input.txt", "ba"));
-        assertEquals(exception.getMessage(), "Can't open the file");
+    public void testWithRussianLetters() throws IOException {
+        List<List<Integer>> expected = List.of(Arrays.asList(0, 7));
+        List<List<Integer>> actual = SubStringFinder.findSubString("input_rus.txt", "пирог");
+        assertEquals(actual, expected);
     }
 
     @Test
-    public void test() throws IOException {
-        ArrayList<Integer> expected = new ArrayList<>();
-        expected.add(0);
-        expected.add(2);
-        expected.add(4);
-        expected.add(6);
-        List<Integer> actual = SubStringFinder.findSubString("input1.txt", "aba");
-        assertEquals(actual,expected);
+    public void emptyAnswerTest() throws IOException {
+        assertTrue(SubStringFinder.findSubString("input_rus.txt", "слово").isEmpty());
+    }
+
+    @Test
+    public void anotherTest() throws IOException {
+        List<List<Integer>> expected =
+                List.of(Arrays.asList(0, 0), Arrays.asList(0, 2), Arrays.asList(0, 4),
+                        Arrays.asList(0, 6), Arrays.asList(1, 0), Arrays.asList(1, 2),
+                        Arrays.asList(2, 5));
+        List<List<Integer>> actual = SubStringFinder.findSubString("input1.txt", "dad");
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void warAndPeaceTest() throws IOException {
+        List<List<Integer>> actual = SubStringFinder.findSubString("WarAndPeace.txt", "bragging");
+        List<List<Integer>> expected = List.of(Arrays.asList(1564, 19), Arrays.asList(7481, 10));
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void bigFileTest() throws IOException {
+        FileCreator.createFile("bigFile.txt");
+        List<List<Integer>> actual = SubStringFinder.findSubString("bigFile.txt", "aba");
+        List<List<Integer>> expected = List.of(Arrays.asList(0, 12), Arrays.asList(10000001, 12));
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void exceptionTest() {
+        Throwable exception = assertThrows(NullPointerException.class, () -> SubStringFinder.findSubString("empty_input.txt", "ba"));
+        assertEquals(exception.getMessage(), "File is empty");
+        exception = assertThrows(IOException.class, () -> SubStringFinder.findSubString("wrong_input.txt", "ba"));
+        assertEquals(exception.getMessage(), "Can't open the file");
     }
 }
