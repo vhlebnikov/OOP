e205f943e1a323394551f2e3a4882c59b2aacb4f
vhlebnikov <v.khlebnikov@g.nsu.ru>
2022-12-16 01:44:21 +0700

    Reviewdog fixed + added previous tasks

---
+++ Task_1_2_2/src/main/java/ru/nsu/khlebnikov/DeepFirstSearchIterator.java
@@ -0,0 +1,45 @@
+package ru.nsu.khlebnikov;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.Stack;
+
+/**
+ * My realization of Deep First Search Iterator.
+ *
+ * @param <T> type parameter
+ */
+public class DeepFirstSearchIterator<T> implements Iterator<T> {
+    private final int modCounter;
+    private Stack<Tree<T>> stack = new Stack<Tree<T>>();
+
+    /**
+     * Realization of Iterator for Deep First Search Iterator that
+     * uses stack to traverse the tree.
+     * Traverses the tree according to {@link #next} and {@link #hasNext} methods.
+     *
+     * @param root root of the tree
+     */
+    public DeepFirstSearchIterator(Tree<T> root) {
+        stack.add(root);
+        modCounter = root.getModCounter();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    @Override
+    public T next() {
+        Tree<T> node = stack.pop();
+        if (modCounter != node.getModCounter()) {
+            throw new ConcurrentModificationException("You mustn't change"
+                    + "tree while iterator is on");
+        }
+        for (Tree<T> n : node.getChildren()) {
+            stack.push(n);
+        }
+        return node.getData();
+    }
+}
