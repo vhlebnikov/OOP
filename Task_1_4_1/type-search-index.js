697f3ced36f08c9018fe13b104c7c7770f0fff4f
vhlebnikov <vhlebnikov@users.noreply.github.com>
2022-12-18 19:39:07 +0000

    Deploying to gh-pages from @ vhlebnikov/OOP@07174998111f14ccb57d80818401eff62de3c1a9 🚀

--- Task_1_4_1/type-search-index.js
+++ Task_1_4_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.khlebnikov","l":"GradeBook"},{"p":"ru.nsu.khlebnikov","l":"Subject"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.khlebnikov","l":"GradeBook"},{"p":"ru.nsu.khlebnikov","l":"Mark"},{"p":"ru.nsu.khlebnikov","l":"Subject"}];updateSearchResults();
