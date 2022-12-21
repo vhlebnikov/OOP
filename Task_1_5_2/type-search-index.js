3e0b9936db990ae38000855aa280cd7989245886
vhlebnikov <vhlebnikov@users.noreply.github.com>
2022-12-21 14:38:50 +0000

    Deploying to gh-pages from @ vhlebnikov/OOP@b7e581cc923561d07a4d70d282d6188d0a18ad8a 🚀

---
+++ Task_1_5_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.khelbnikov","l":"Note"},{"p":"ru.nsu.khelbnikov","l":"NoteBook"}];updateSearchResults();
