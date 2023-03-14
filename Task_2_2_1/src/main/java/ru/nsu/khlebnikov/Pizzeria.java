1ab1c062106c5027aa2a08939952e8ba4c0823ca
Вадим Хлебников <v.khlebnikov@g.nsu.ru>
2023-03-14 10:02:24 +0700

    ArrayBlockingQueue --> LinkedBlockingQueue

--- Task_2_2_1/src/main/java/ru/nsu/khlebnikov/Pizzeria.java
+++ Task_2_2_1/src/main/java/ru/nsu/khlebnikov/Pizzeria.java
@@ -9,6 +9,7 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 public class Pizzeria {
@@ -22,7 +23,7 @@ public class Pizzeria {
     public Pizzeria(String fileName) throws IOException {
         JsonData jsonData = new JsonData(fileName);
         storage = new LinkedBlockingDeque<>(jsonData.getStorageCapacity());
-        orderQueue = new ArrayBlockingQueue<>(100);
+        orderQueue = new LinkedBlockingQueue<>();
         this.bakers = jsonData.getBakers();
         this.deliverymen = jsonData.getDeliverymen();
         bakersPool = Executors.newFixedThreadPool(bakers.size());
